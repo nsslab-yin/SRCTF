5395ce28b30162b8d34daa5c85db9e8f9ec0f1d7
yinyue <yinyue@uga.edu>
2016-11-09 10:37:19 -0500

    Clean django framework

---
+++ pswd_generator.py
@@ -0,0 +1,29 @@
+import MySQLdb
+import random
+import string
+
+
+def pswd_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+db = MySQLdb.connect("localhost","djdb","django","djdb" )
+
+cursor = db.cursor()
+
+#cursor.execute("SELECT * from reuse_category")
+
+#data = cursor.fetchall()
+#for line in data:
+#    print line
+
+for i in range(1, 100):
+    try:
+        ran_pswd = pswd_generator()
+        print ran_pswd
+        cursor.execute("""INSERT INTO reuse_pswd_table VALUES (%s, %s)""", (i, ran_pswd))
+        db.commit()
+    except:
+        print "what?"
+        db.rollback()
+    
+db.close()
